23021dd9305001f74e055024ba4a79359524a619
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2022-10-13 21:51:14 -0300

    feat: added font and first css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-export function App() {
-  
+import './styles/global.css';
+
+export function App() {  
   return (
-    <h1>oi</h1>
+    <>
+      <h1 className='font-bold text-5xl text-violet-500'>DS do Luisinho</h1>
+
+      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>Enviar</button>
+    </>
   )
 }
 
